6df1bc67c566d74b901898515f9c13c342df8ca5
retr0lbb <retr0lbb@gmail.com>
2025-07-08 16:22:43 -0300

    feat: start server

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import { fastify } from "fastify"
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
+import { fastifyCors } from "@fastify/cors"
+
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+    origin: "*"
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.listen({port: 3333}).then(() => {
+    console.log("Http server Running")
+})
